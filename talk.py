992403abf6e21a0c835d3377aaa29672de48bdfd
Akshay Tiwari <akshay@Akshays-MacBook-Air.local>
2025-09-15 10:30:15 +0200

    Added gracefull shutdowns and saftey features like timeouts

--- talk.py
+++ talk.py
@@ -1,12 +1,14 @@
 import galtea
 import requests
 import json
+import signal
 from dotenv import load_dotenv
 from twilio.rest import Client
 from galtea import Galtea
 import os,uuid,time
 
 load_dotenv()
+sid = None
 
 class MyAgent(galtea.Agent):
     def __init__(self,remote_url,from_number,to_number):
@@ -19,6 +21,10 @@ class MyAgent(galtea.Agent):
         self.remote_url = remote_url
         self.from_number = from_number
         self.to_number = to_number
+        self.asycio_timeout = 120.0
+        self.request_timeout = 120.0
+        self.sid = None
+        self.conversation_ended = False
 
     def start_call(self):
         
@@ -28,43 +34,70 @@ class MyAgent(galtea.Agent):
             url=f"{self.remote_url}/twilio-voice",
         )
         print(f"Call started with sid: {self.call_twilio.sid}")
+        global sid
+        sid = self.call_twilio.sid
 
     def end_call(self):
         self.client.calls(f"{self.call_twilio.sid}").update(status='completed')
 
     def generate_(self,first, timeout, input):
-        params_first_call = { "first": first,  "timeout": timeout, "input": input }
+        params_first_call = {"client":self.client, "sid": self.call_twilio.sid, "first": first,  "timeout": timeout, "input": input, "talk_timeout": 80 }
         try:
-            response_first = requests.get( f"{self.BASE_URL}/generate",  headers=self.headers, params=params_first_call )
+            response_first = requests.get( f"{self.BASE_URL}/generate",  headers=self.headers, params=params_first_call, timeout=self.request_timeout )
             if response_first.status_code == 200:
                 print("Response JSON:", response_first.json())
                 return response_first.json()
             elif response_first.status_code == 204:
-                print("Response: No content (Timeout reached, no user speech detected).")
+                print("Response: No content (Timeout reached, no user speech detected). Exiting main script.")
+                exit(0)
             else:
                 print("Error Response:", response_first.text)
+                print("Exiting main script.")
+                exit(0)
+
         except requests.exceptions.RequestException as e:
             print(f"An error occurred: {e}")
-
+            print("Exiting main script.")
+            exit(0)
 
     def call(self, input_data: galtea.AgentInput) -> galtea.AgentResponse:
 
         if len(input_data.messages) ==0:
             self.start_call()
             print("call started")
-            response = self.generate_(True, 30.0, "")
+            response = self.generate_(True, self.asycio_timeout, "")
             print("response generated")
         else:
-            print("generating response with user message")
-            user_message = input_data.last_user_message_str()
-            print("generating response with user message1")
-
-            response = self.generate_(False, 30.0, user_message)
-            print("generating response with user message2")
-
+            if not self.conversation_ended:
+                print("generating response with user message")
+                user_message = input_data.last_user_message_str()
+                response = self.generate_(False, self.asycio_timeout, user_message)
+                if response["response"] == "Conversation ended by Twilio":
+                    self.conversation_ended = True
+            else:
+                response = {}
+                response["response"] = "Conversation ended by Twilio"
 
         return galtea.AgentResponse( content=response["response"],  metadata={"model_version": "1.0"} )
 
+
+def shutdown_handler(signum, frame):
+    """
+    This function is our custom signal handler.
+    It will be executed when this script receives SIGINT or SIGTERM.
+    """
+    print(f"\nCaught signal {signum}. Initiating graceful shutdown...")
+    if sid:
+        account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+        auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+        client = Client(account_sid, auth_token)
+        client.calls(f"{agent.call_twilio.sid}").update(status='completed')
+        print(" Twilio call status updated to completed")
+    print("Exiting main script.")
+    exit(0)
+
+for sig in (signal.SIGTERM, signal.SIGINT):
+    signal.signal(sig, shutdown_handler)
 
 
 
@@ -80,9 +113,12 @@ for i, test_case in enumerate(test_cases):
 
         time.sleep(2)
         # +34960324442
-        agent = MyAgent(remote_url="https://kosh6r5673fg71.ngrok-free.app",
+        agent = MyAgent(remote_url="https://d132dd45a2cd.ngrok-free.app",
         from_number="+12136957366",to_number="+34960324442")
         uid = str(uuid.uuid4())
+
+        
+
 
         # Create a session for this test case
         session = galtea_client.sessions.create(
